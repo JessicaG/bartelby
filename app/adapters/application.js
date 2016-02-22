c219dc0c8363a3495de6912a8a03bb03eda310b5
jessicag <jgoulding2@gmail.com>
2016-02-22 11:44:34 -0700

    initial commit with bartelby project

---
+++ app/adapters/application.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+// let fakeData = [
+//   { id: 'hello.md', content: 'This is a note.' },
+//   { id: 'byebye.md', content: 'This is another note.' },
+// ];
+const electron = requireNode('electron');
+const mainProcess = electron.remote.require('./electron');
+const filesystem = mainProcess.filesystem;
+
+export default DS.Adapter.extend({
+
+  findAll() {
+    return filesystem.all();
+  }
+
+});
---
+++ electron.js
@@ -0,0 +1,41 @@
+/* jshint node: true */
+'use strict';
+
+const electron         = require('electron');
+const FileBin          = require('file-bin');
+
+const app              = electron.app;
+const BrowserWindow    = electron.BrowserWindow;
+const emberAppLocation = `file://${__dirname}/dist/index.html`;
+
+let mainWindow = null;
+let filesystem = new FileBin(__dirname + '/notes', ['.txt', '.md', '.markdown']);
+
+electron.crashReporter.start();
+
+app.on('window-all-closed', function onWindowAllClosed() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function onReady() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  delete mainWindow.module;
+
+  mainWindow.loadURL(emberAppLocation);
+
+  mainWindow.webContents.on('did-fail-load', () => {
+    mainWindow.loadURL(emberAppLocation);
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+exports.filesystem = filesystem;
