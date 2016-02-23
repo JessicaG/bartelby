f4104d27e56552b6eea3e5f0275b726a17cb3fbb
Jessica Goulding and Jill Donohue <pair+jessicag+jillmd501@pivotallabs.com>
2016-02-23 14:32:51 -0700

    removed bugs from editing content - Titles cannot be edited on purpose

--- app/controllers/notes/note.js
+++ app/controllers/notes/note.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    changeValue (val) {
-      this.set('value', value)
-    },
     saveNote() {
       this.get('model').save();
     }
